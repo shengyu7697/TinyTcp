7c7f2deb76ce6a587f23139cc1eb593b70f32637
shengyu <shengyu7697@gmail.com>
2018-05-08 15:45:11 +0800

    Return ip addr string in getEthIpAddr()

--- src/TinyTcp/NetworkUtil.cpp
+++ src/TinyTcp/NetworkUtil.cpp
@@ -5,7 +5,8 @@
 #include <sys/socket.h>
 #include <sys/ioctl.h>
 #include <net/if.h> // for struct ifreq
-#include <arpa/inet.h> // for inet_ntoa
+#include <arpa/inet.h> // for inet_ntoa/inet_ntop
+#include <netinet/in.h> // for INET_ADDRSTRLEN
 
 std::string getEthIpAddr(const char *ifname)
 {
@@ -13,18 +14,14 @@ std::string getEthIpAddr(const char *ifname)
     struct ifreq ifr;
 
     fd = socket(AF_INET, SOCK_DGRAM, 0);
-
-    // get an IPv4 IP address
-    ifr.ifr_addr.sa_family = AF_INET;
-
-    // I want IP address attached to "eth0"
-    strncpy(ifr.ifr_name, ifname, IFNAMSIZ-1);
-
+    ifr.ifr_addr.sa_family = AF_INET; // get an IPv4 IP address
+    strncpy(ifr.ifr_name, ifname, IFNAMSIZ-1); // attached to "eth0"
     ioctl(fd, SIOCGIFADDR, &ifr);
-
     close(fd);
 
-    printf("%s\n", inet_ntoa(((struct sockaddr_in *)&ifr.ifr_addr)->sin_addr));
+    //printf("%s\n", inet_ntoa(((struct sockaddr_in *)&ifr.ifr_addr)->sin_addr));
+    char buf[INET_ADDRSTRLEN];
+    inet_ntop(AF_INET, &( (struct sockaddr_in *)&ifr.ifr_addr )->sin_addr, buf, INET_ADDRSTRLEN);
 
-    return std::string("");
+    return std::string(buf);
 }
