73dd1c139b403d761c56abba88a8f858fe791c84
shengyu <shengyu7697@gmail.com>
2018-11-30 23:57:51 +0800

    Add testPacketLost to udp

--- src/test_udp_client/client.cpp
+++ src/test_udp_client/client.cpp
@@ -7,6 +7,47 @@
 
 using namespace std;
 
+void testStdin(const std::string &hostname, const int &port)
+{
+	TinyUdpClient uc;
+	uc.start(hostname, port);
+
+	char msg[256];
+	while (uc.isRunning())
+	{
+		cin >> msg;
+		if (strcmp(msg, "q") == 0) {
+			uc.stop();
+		} else {
+			uc.send(msg, (int)strlen(msg));
+		}
+	}
+}
+
+void testPacketLost(const std::string &hostname, const int &port)
+{
+	TinyUdpClient uc;
+	uc.start(hostname, port);
+
+	int count = 1;
+
+	char msg[256];
+	while (uc.isRunning())
+	{
+		sprintf(msg, "Hello Wrold! %d", count);
+		uc.send(msg, (int)strlen(msg));
+		printf("send %s ...\n", msg);
+
+		if (count == 1000) {
+			uc.stop();
+			break;
+		}
+		count++;
+
+		//sleep_ms(1000 * 1);
+	}
+}
+
 int main(int argc, char* argv[])
 {
     string hostname = "127.0.0.1";
@@ -22,25 +63,8 @@ int main(int argc, char* argv[])
 
     printf("start client, connect to %s:%d\n", hostname.c_str(), port);
 
-    TinyUdpClient uc;
-    uc.start(hostname, port);
-
-    char msg[256];
-    while (uc.isRunning())
-    {
-        cin >> msg;
-        if (strcmp(msg, "q") == 0) {
-            uc.stop();
-        } else {
-            uc.send(msg, (int)strlen(msg));
-        }
-
-        /*strcpy(msg, "Hello Wrold!");
-        uc.send(msg, (int)strlen(msg));
-        printf("send %s ...\n", msg);
-
-        sleep_ms(1000 * 1);*/
-    }
+    //testStdin(hostname, port);
+    testPacketLost(hostname, port);
 
     printf("end of process.\n");
 
--- src/test_udp_server/server.cpp
+++ src/test_udp_server/server.cpp
@@ -12,6 +12,53 @@ void onRecv(const char *buf, int len)
     printf("Client: %s\n", buf);
 }
 
+void onRecvTestPacketLost(const char *buf, int len)
+{
+	static int count = 1;
+    printf("count=%d data=%s\n", count, buf);
+    count++;
+    //sleep_ms(1000 * 1000);
+}
+
+void testStdin(const int &port)
+{
+	TinyUdpServer us;
+	us.setOnRecv(onRecv);
+	us.start(port);
+
+	char msg[256];
+	while (us.isRunning())
+	{
+		cin >> msg;
+		if (strcmp(msg, "q") == 0) {
+			us.stop();
+		} /*else {
+			if (server.isConnected()) {
+				//us.send(msg, (int)strlen(msg)); // send to connection 0 // FIXME send to cur connection
+				//us.send(1, msg, (int)strlen(msg)); // send to connection i
+				us.sendAll(msg, (int)strlen(msg)); // send to all connection
+			}
+		}*/
+		//sleep_ms(1000 * 1);
+	}
+}
+
+void testPacketLost(const int &port)
+{
+	TinyUdpServer us;
+	us.setOnRecv(onRecvTestPacketLost);
+	us.start(port);
+
+	char msg[256];
+	while (us.isRunning())
+	{
+		cin >> msg;
+		if (strcmp(msg, "q") == 0) {
+			us.stop();
+		}
+	}
+}
+
 int main(int argc, char* argv[])
 {
     int port = 7000;
@@ -25,25 +72,8 @@ int main(int argc, char* argv[])
 
     printf("start server, bind on %d\n", port);
 
-    TinyUdpServer us;
-    us.setOnRecv(onRecv);
-    us.start(port);
-
-    char msg[256];
-    while (us.isRunning())
-    {
-        cin >> msg;
-        if (strcmp(msg, "q") == 0) {
-            us.stop();
-        } /*else {
-            if (server.isConnected()) {
-                //us.send(msg, (int)strlen(msg)); // send to connection 0 // FIXME send to cur connection
-                //us.send(1, msg, (int)strlen(msg)); // send to connection i
-                us.sendAll(msg, (int)strlen(msg)); // send to all connection
-            }
-        }*/
-        //sleep_ms(1000 * 1);
-    }
+    //testStdin(port);
+    testPacketLost(port);
 
     printf("end of process.\n");
 
